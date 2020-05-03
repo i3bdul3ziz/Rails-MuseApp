25056684e3c456531eab8ca56731199af2aa01e1
Schnecke <eiweala@live.com>
2020-05-03 14:15:15 +0300

    completed onetomany

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,12 +1,11 @@
 class ArtistsController < ApplicationController
+
+  # before_action :set_artist, only: [:show]
+
   def index
     @artists = Artist.all
     # render json: @artists
     # render json: {"message": "Index page loaded"}
-  end
-
-  def show
-    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -17,22 +16,42 @@ class ArtistsController < ApplicationController
     # params.require(:artist).permit
     # Whitelisting
     # Strong parameters
-    Artist.create(params.require(:artist).permit(:name, :hometown, :img))
+    Artist.create(artist_params)
     redirect_to artists_path
   end
 
   def edit
+
     @artist = Artist.find(params[:id])
+  end
+
+  def show
+    begin
+      @artist = Artist.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to artists_path
+      
+    end
   end
 
   def update
     artist = Artist.find(params[:id])
-    artist.update(params.require(:artist).permit(:name, :hometown, :img))
+    artist.update(artist_params)
     redirect_to artists_path
   end
 
   def delete
     Artist.find(params[:id]).destroy
     redirect_to artists_path
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :hometown, :img)
   end
 end
